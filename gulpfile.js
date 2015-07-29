c4c0c45b02e4cb1feec6343bc6e37b3d36bf6598
Steven Scales <design@stevescales.com>
2015-07-28 23:28:17 -0400

    recreated new design and new layout complete with all the set up, removed neat

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,26 @@
 var gulp = require('gulp'),
   rename = require('gulp-rename'),
     sass = require('gulp-sass'),
-    maps = require('gulp-sourcemaps');
+    maps = require('gulp-sourcemaps'),
+  please = require('gulp-pleeease');
+
+var PleeeaseOptions = {
+    optimizers: {
+        minifier: false
+    }
+};
 
 gulp.task('compileSass', function() {
     gulp.src("scss/style.scss")
         .pipe(maps.init())
         .pipe(sass())
         .pipe(maps.write('./'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', function () {
+    gulp.src('./scss/style.scss')
+        .pipe(sass({sourceComments: 'map'}))
+        .pipe(please(PleeeaseOptions))
         .pipe(gulp.dest('css'));
 });
