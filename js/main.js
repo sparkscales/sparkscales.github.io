ada4a15dab704f92174b99ea39c136a3ce2812f8
Steven Scales <design@stevescales.com>
2015-08-30 02:13:52 -0400

    Added a projects a bunch of media quiery, sticky head, and transition issues. A lot of transitions and layout modification

--- js/main.js
+++ js/main.js
@@ -11,19 +11,55 @@
 //   $grid.isotope('layout');
 // });
 
-$(document).ready( function() {
+// $(document).ready( function() {
+//
+// var $grid = $('.portfolio').imagesLoaded( function() {
+//   // init Isotope after all images have loaded
+//   $grid.isotope({
+//     itemSelector: '.portfolio__item',
+//     transitionDuration: '0.6s',
+//     masonry: {
+//       columnWidth: 320
+//     }
+//     });
+//   });
+//   $('#isotope_filters li a').on('click', function() {
+//     var selector = $(this).data('filter');
+//     $grid.isotope({
+//       filter: selector
+//     });
+//   });
+// });
 
-var $grid = $('.portfolio').imagesLoaded( function() {
-  // init Isotope after all images have loaded
-  $grid.isotope({
-    itemSelector: '.portfolio__item',
-    transitionDuration: '0.6s',
-    masonry: {
-      columnWidth: 320
-    }
+$(window).load(function() {
+
+    // cache container
+    var $container = $('.portfolio').imagesLoaded( function() {
+
+      // initialize isotope
+      $container.isotope({
+      // options...
+      animationEngine: 'best-available',
+      itemSelector: '.portfolio__item',
+      packery: {
+        columnWidth: 320
+      }
+      });
     });
+
+    // filter items when filter link is clicked
+    $('#isotope_filters li a').on('click', function() {
+    var selector = $(this).attr('data-filter');
+    $container.isotope({
+    filter: selector
+    });
+
   });
 });
+
+
+
+
 
 $(document).ready(function() {
 
@@ -32,11 +68,11 @@ $(document).ready(function() {
   });
 });
 
-$(window).scroll(function() {
-if ($(this).scrollTop() > 120){
-    $('body').addClass("sticky");
-  }
-  else{
-    $('body').removeClass("sticky");
-  }
-});
+// $(window).scroll(function() {
+// if ($(this).scrollTop() > 0){
+//     $('body').addClass("sticky");
+//   }
+//   else{
+//     $('body').removeClass("sticky");
+//   }
+// });
