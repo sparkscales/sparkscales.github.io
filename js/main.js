c91314d1c7ced80988fe2d56c1691dcf188dcfb3
Steven Scales <design@stevescales.com>
2015-08-30 23:32:15 -0400

    created portfolio pages, added images, cleaned up sass, and made some minor layout tweaks

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
-// var $grid = $('.portfolio').isotope({
-//   itemSelector: '.portfolio__item',
-//   transitionDuration: '0.6s',
-//   masonry: {
-//     columnWidth: 320
-//   }
-//     // layoutMode: 'packery'
-// });
-// // layout Isotope after each image loads
-// $grid.imagesLoaded().progress( function() {
-//   $grid.isotope('layout');
-// });
-
-// $(document).ready( function() {
-//
-// var $grid = $('.portfolio').imagesLoaded( function() {
-//   // init Isotope after all images have loaded
-//   $grid.isotope({
-//     itemSelector: '.portfolio__item',
-//     transitionDuration: '0.6s',
-//     masonry: {
-//       columnWidth: 320
-//     }
-//     });
-//   });
-//   $('#isotope_filters li a').on('click', function() {
-//     var selector = $(this).data('filter');
-//     $grid.isotope({
-//       filter: selector
-//     });
-//   });
-// });
+// ISOTOPE
 
 $(window).load(function() {
 
@@ -53,24 +22,11 @@ $(window).load(function() {
     $container.isotope({
     filter: selector
     });
-
   });
 });
 
-
-
-$(document).ready(function() {
+// FILTER TOGGLE
 
   $('#mobile-button').click(function(event) {
     $('body').toggleClass('is-open');
   });
-});
-
-// $(window).scroll(function() {
-// if ($(this).scrollTop() > 0){
-//     $('body').addClass("sticky");
-//   }
-//   else{
-//     $('body').removeClass("sticky");
-//   }
-// });
